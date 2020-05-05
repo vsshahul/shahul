952bcddff0dd13b4d83751a02b5ca3de403cd1f0
shahul hameed vs <shahul@f22labs.com>
2020-05-05 17:27:10 +0530

    ...

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,28 +4,20 @@ import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
+import '../page-styles/projects.scss'
+
 const Nav = () => (
     <Layout>
     <SEO title="ui8.dev" />
     <article className='project'>
-      <h1 className=''>
+      <h1 className='h1'>
         <a href='https://www.ui8.dev/'>UI8</a> 
       </h1>
       <div className='link-cont'>
-        <ul>
-          <li>
-            <Link to="/components/checkout">Checkout Container</Link>
-          </li>
-          <li>
-            <Link to="/components/heroContainer">HeroContainer</Link>
-          </li>
-          <li>
-            <Link to="/components/pictureCont">Picture Container</Link>
-          </li>
-          <li>
-            <Link to="/">Go back</Link>
-          </li>
-        </ul>
+        <Link to="/components/checkout">Checkout Container</Link>
+        <Link to="/components/heroContainer">HeroContainer</Link>
+        <Link to="/components/pictureCont">Picture Container</Link>
+        <Link to="/">Go back</Link>
       </div>
     </article>
 
